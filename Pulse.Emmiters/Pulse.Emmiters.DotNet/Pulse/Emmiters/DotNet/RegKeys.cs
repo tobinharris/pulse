3f80029199959b9b90f19d4f3f880ef30f1168c5
unknown <Tobin Harris@.(none)>
2009-07-24 15:43:52 +0100

    big refactor

--- Pulse.Emmiters/Pulse.Emmiters.DotNet/Pulse/Emmiters/DotNet/RegKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pulse.Emmiters.DotNet
-{
-    public static class RegKeys
-    {
-        public const string PER_MVC_ACTION_REQUEST = "per_mvc_action_request";
-        public const string PER_REQUEST = "per_request";
-        public const string TIME_BASED = "time_based";
-    }
-}
